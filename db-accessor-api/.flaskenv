efa011dae7df6feaa277eba7b3121f904ff27650
RobertSmithers <rjsmithers3@gmail.com>
2024-10-12 18:55:34 -0500

    Removed all microservices to accomodate migration to git submodules

--- db-accessor-api/.flaskenv
+++
@@ -1,4 +0,0 @@
-FLASK_APP=web-service.py
-FLASK_ENV=development
-FLASK_RUN_PORT=443
-FLASK_DEBUG=true
--- server/generate-certs.sh
+++
@@ -1,5 +0,0 @@
-
-# Generate server key and certificate
-openssl req -newkey rsa:2048 -nodes -keyout server.key -x509 -days 365 -out server.crt
-
-
